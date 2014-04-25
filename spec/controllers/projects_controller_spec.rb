17020f8744b6441ddead510c8d4ccf7e7a750f40
David G Chaves <davidgchaves@gmail.com>
2014-04-25 19:53:46 +0200

    Alert and redirect the users if they try to access a project that doesn't exist

--- spec/controllers/projects_controller_spec.rb
+++ spec/controllers/projects_controller_spec.rb
@@ -1,5 +1,17 @@
 require 'spec_helper'
 
 describe ProjectsController do
+  context "when attempting to access a resource that no longer exists" do
+    before do
+      get :show, id: 'resource-that-no-longer-exists'
+    end
 
+    it "redirects to the Projects page" do
+      expect(response).to redirect_to projects_path
+    end
+
+    it "alerts with the appropriate message" do
+      expect(flash[:alert]).to eql "The project you were looking for could not be found."
+    end
+  end
 end
