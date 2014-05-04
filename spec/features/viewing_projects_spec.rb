8ba92e910ac305b5f7d309f2f5948571733919ec
David G Chaves <davidgchaves@gmail.com>
2014-05-04 13:05:53 +0200

    Don't show projects that a user doesn't have permission to see

--- spec/features/viewing_projects_spec.rb
+++ spec/features/viewing_projects_spec.rb
@@ -3,18 +3,33 @@ require 'spec_helper'
 feature "Viewing Projects" do
   let!(:user) { FactoryGirl.create :user }
   let!(:project) { FactoryGirl.create :project }
+  let!(:hidden_project) { FactoryGirl.create :project, name: "Hidden" }
 
   context "A signed in user" do
-    before do
-      sign_in_as! user
-      define_permission! user, :view, project
+    before { sign_in_as! user }
+
+    context "with permission to view a project" do
+      before do
+        define_permission! user, :view, project
+        visit "/"
+      end
+
+      scenario "can list the project" do
+        expect(page).to have_content project.name
+      end
+
+      scenario "can navigate to the project" do
+        click_link project.name
+        expect(page.current_url).to eq project_url(project)
+      end
     end
 
-    scenario "can list all projects" do
-      visit "/"
-      click_link project.name
+    context "with no permission to view a project" do
+      before { visit "/" }
 
-      expect(page.current_url).to eq project_url(project)
+      scenario "can't list the project" do
+        expect(page).to_not have_content hidden_project.name
+      end
     end
   end
 end
