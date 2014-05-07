2fc22dd1c7b75636b8af74be34b3d3b3792895d5
David G Chaves <davidgchaves@gmail.com>
2014-05-07 12:38:57 +0200

    Uploaded assets are now strictly served through the FilesController

--- app/models/asset.rb
+++ app/models/asset.rb
@@ -1,3 +1,13 @@
 class Asset < ActiveRecord::Base
   mount_uploader :asset, AssetUploader
+
+  belongs_to :ticket
+
+  before_save :update_content_type
+
+  private
+
+    def update_content_type
+      self.content_type = asset.file.content_type if asset.present? && asset_changed?
+    end
 end
---
+++ spec/controllers/files_controller_spec.rb
@@ -0,0 +1,41 @@
+require 'spec_helper'
+
+describe FilesController do
+  let(:good_user) { FactoryGirl.create :user }
+  let(:bad_user) { FactoryGirl.create :user }
+  let(:project) { FactoryGirl.create :project }
+  let(:ticket) { FactoryGirl.create :ticket, project: project, user: good_user }
+  let(:path) { Rails.root + "spec/fixtures/speed.txt" }
+  let(:asset) { ticket.assets.create asset: File.open(path) }
+
+  context "When a signed in user with 'view' (project) permission" do
+    before do
+      sign_in good_user
+      good_user.permissions.create! action: "view", thing: project
+    end
+
+    context "tries to access a project's asset" do
+      before { get :show, id: asset.id }
+
+      it "succeeds" do
+        expect(response.body).to eq File.read(path)
+      end
+    end
+  end
+
+  context "When a signed in user with no 'view' (project) permission" do
+    before { sign_in bad_user }
+
+    context "tries to access a project's asset" do
+      before { get :show, id: asset.id }
+
+      it "gets redirected to the root page" do
+        expect(response).to redirect_to root_path
+      end
+
+      it "gets alerted the asset could not be found" do
+        expect(flash[:alert]).to eq "The asset you were looking for could not be found."
+      end
+    end
+  end
+end
