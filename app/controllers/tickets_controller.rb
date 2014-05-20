c5f9b5dcf9e86e8c62479ec500cb9c3b3f4aed6f
David G Chaves <davidgchaves@gmail.com>
2014-05-20 14:05:41 +0200

    Fix typo in tickets_controller.rb

--- app/controllers/tickets_controller.rb
+++ app/controllers/tickets_controller.rb
@@ -3,8 +3,8 @@ class TicketsController < ApplicationController
   before_action :find_project
   before_action :find_ticket, only: [:show, :edit, :update, :destroy, :watch]
   before_action :authorize_create!, only: [:new, :create]
-  before_action :authorize_update!, only: [:edit, :update, :update]
-  before_action :authorize_delete!, only: :destroy
+  before_action :authorize_update!, only: [:edit, :update]
+  before_action :authorize_delete!, only: [:destroy]
 
   def new
     @ticket = @project.tickets.build
